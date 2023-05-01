e8af0befd262e06ef6b8d3a3169932811ff91315
Dellucifer <pg99285@gmail.com>
2023-05-02 01:40:34 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="app">
-      
+      <Header />
+      <Home />
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
-    <div className='header'>
-      <img className="header__logo" src="" alt="" />
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://raw.githubusercontent.com/dellucifer/Apni-Dukaan/master/public/logo_apni_dukaan.png"
+        alt=""
+      />
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__option__top">Hello Guest</span>
+          <span className="header__option__bottom">Sign In</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__option__top">Returns</span>
+          <span className="header__option__bottom">& Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__option__top">Your</span>
+          <span className="header__option__bottom">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__option__bottom header__basketCount">0</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
