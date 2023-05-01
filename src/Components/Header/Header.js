e59d97b0f1a994b10020e0bd8a8b1b79d7fd27ed
Dellucifer <pg99285@gmail.com>
2023-05-02 00:14:06 +0530

    Initial commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <img className="header__logo" src="" alt="" />
+    </div>
+  )
+}
+
+export default Header
